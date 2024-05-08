0ad04a60780e588a89cc51636ba62eb9e75fe6d3
skills nwokolo anthony <skillsnwokolo372@gmail.com>
2024-05-08 00:31:40 -0230

    fixed installation error

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nh=gypq10^dgopxjw0bpb^s9%8m9_59#qviec-yxat$r7!0i7o"
+SECRET_KEY = "django-insecure-1%2l-lits5mbtpo)y@=bvosu^+#b_f!1p3w(2@a#)m0j80p&_t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "_items"
 ]
 
 MIDDLEWARE = [
